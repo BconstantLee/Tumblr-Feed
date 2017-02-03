63fccea2714233217193543dd6ab81f9b159d8a6
Bconstantlee <bconstantlee@gmail.com>
2017-02-02 19:01:17 -0500

    Done

---
+++ TumblrFeed/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Bconsatnt on 2/2/17.
+//  Copyright © 2017 Bconsatnt. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var imageCell: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
